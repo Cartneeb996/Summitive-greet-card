45db01982930c63a6741b3c1f9a500bc8e5c1aa6
Cartneeb996 <CartNeeb996@ed.amdsb.ca>
2017-09-19 13:35:06 -0400

    Add project files.

---
+++ Summitive greet card/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Summitive_greet_card
+{
+    partial class greetingCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.greetTitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(12, 9);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(471, 55);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Hello, It\'s me; Carter!";
+            // 
+            // greetTitleLabel
+            // 
+            this.greetTitleLabel.AutoSize = true;
+            this.greetTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greetTitleLabel.Location = new System.Drawing.Point(121, 64);
+            this.greetTitleLabel.Name = "greetTitleLabel";
+            this.greetTitleLabel.Size = new System.Drawing.Size(199, 29);
+            this.greetTitleLabel.TabIndex = 1;
+            this.greetTitleLabel.Text = "Happy Greetings!";
+            // 
+            // greetingCardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 351);
+            this.Controls.Add(this.greetTitleLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "greetingCardForm";
+            this.Text = "Hello, I\'m a Greeting Card!";
+            this.Click += new System.EventHandler(this.greetingCardForm_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label greetTitleLabel;
+    }
+}
+
---
+++ Summitive greet card/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Summitive_greet_card
+{
+    public partial class greetingCardForm : Form
+    {
+        public greetingCardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void greetingCardForm_Click(object sender, EventArgs e)
+        {
+            greetTitleLabel.Visible = false;
+            nameLabel.Visible = false;
+
+            Graphics gemini = this.CreateGraphics();
+            SolidBrush starDots = new SolidBrush(Color.White);
+
+            gemini.Clear(Color.Black);
+            gemini.FillEllipse(starDots, 67, 63, 4, 4);
+            gemini.FillEllipse(starDots, 99, 66, 3, 3);
+            gemini.FillEllipse(starDots, 80, 42, 16, 16);
+            gemini.FillEllipse(starDots, 118, 33, 8, 8);
+            gemini.FillEllipse(starDots, 133, 46, 3, 3);
+            gemini.FillEllipse(starDots, 156, 81, 2, 2);
+            gemini.FillEllipse(starDots, 209, 92, 4, 4);
+            gemini.FillEllipse(starDots, 101, 118, 4, 4);
+            gemini.FillEllipse(starDots, 186, 158, 6, 6);
+            gemini.FillEllipse(starDots, 123, 153, 3, 3);
+            gemini.FillEllipse(starDots, 214, 209, 5, 5);
+            gemini.FillEllipse(starDots, 192, 215, 3, 3);
+            gemini.FillEllipse(starDots, 229, 223, 2, 2);
+            gemini.FillEllipse(starDots, 122, 228, 3, 3);
+            gemini.FillEllipse(starDots, 155, 220, 8, 8);
+
+        }
+    }
+}
